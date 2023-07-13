5fcba7ac50e036c131990b5f34ecea4825823ead
wahid <abdou@gmail.com>
2023-07-13 15:27:55 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 /**
 * main - A program that prints a line with puts function
 *
@@ -10,3 +10,4 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
